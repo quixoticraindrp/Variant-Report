4be6ddfe0b26f2676fbf60ba2b63120a2bf86420
quixoticraindrp <software@quixoticraindrop.com>
2017-07-18 15:22:41 -0700

    Manual attempt

---
+++ Variant-Report/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Variant-Report
+//
+//  Created by Bacil Donovan Warren on 7/18/17.
+//  Copyright © 2017 Quixotic Raindrop Software, LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Variant-Report/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Variant-Report
+//
+//  Created by Bacil Donovan Warren on 7/18/17.
+//  Copyright © 2017 Quixotic Raindrop Software, LLC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
